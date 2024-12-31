b25d40858359d09ebe228808a911913965de086c
Keyner de Ávila <kddeavila3@gmail.com>
2024-12-30 23:52:49 -0500

    v.2 - new design to my portfolio

---
+++ src/types/global.ts
@@ -0,0 +1,13 @@
+declare global {
+    interface CardProps {
+        image: string;
+        title: string;
+        desc: string;
+        tags: string[];
+        github: string;
+        demo: string;
+        timer: number;
+    }
+}
+
+export { };
---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'primary': '#1a1f24',
+				'secondary': '#1d242d',
+				'card': '#2a3038',
+				'dark': '#14181f'
+			},
+			boxShadow: {
+				'back': '0 5px 1px'
+			},
+		},
+	},
+	plugins: [],
+}
